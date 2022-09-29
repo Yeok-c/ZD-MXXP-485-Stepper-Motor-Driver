2479fd23112bf5dda85caebdd5eb297466d7d052
Yeok-c <yeokch@gmail.com>
2022-09-29 14:02:35 +0800

    Revert "Refactored into test_rtu_main.c and zd_485_rtu_master"
    This reverts commit aa6d80c52091ef106d07c085040453ff4924cb45.

--- src/test_rtu_master.c
+++ src/test_rtu_master.c
@@ -1,11 +1,59 @@
-#include "zd_485_rtu_master.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include <errno.h>
+#include "modbus.h"
+#include <assert.h>
+#include <errno.h>
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
 
-int zd_setup(){
-    int ret=0;
+#define PORT_NAME "/dev/ttyUSB1"
+
+// VALUES THAT CAN BE READ
+int REG_ADDRESS   = 3000;
+int REG_MICROSTEP = 3001;
+int REG_POS_MODE  = 3002;
+int REG_ACC_STEP_H  = 3004; // and 3005 TWO BYTES
+int REG_ACC_PARAM_H = 3006; // and 3007 TWO BYTES
+int REG_INIT_PERIOD = 3008;
+int REG_MAX_PERIOD  = 3009;
+int REG_MAX_DIST_H  = 3010; // and 3011 TWO BYTES
+int REG_ZERO_POS_H  = 3012; // and 3013 TWO BYTES
+int REG_LIM_SW_OFST = 3014; // and 3015 TWO BYTES
+
+int read_value(modbus_t *ctx, int reg_addr);
+float read_fvalue(modbus_t *ctx, int reg_addr);
+int num_bytes(int reg_addr);
+long parse_bytes(uint16_t size_four_array[], int nb);
+float parse_bytes_to_float(uint16_t size_four_array[]);
+uint16_t * unparse_2_bytes(long intNumber);
+uint16_t * unparse_float_to_bytes(float f);
+uint16_t * unparse_4_bytes(long intNumber);
+
+int main(int argc, char *argv[])
+{
+    int ret;
+    float retf;
+    uint16_t *table;
+    uint16_t *table__;
+    uint16_t *table_0;
+    uint16_t *table_1;
+    modbus_t *mb;
+    
     char port[20];  
-    strcpy(port, PORT_NAME);
+    printf("argc = %d, argv[1] = %s\n", argc, argv[1]);
+    if(argc == 2)
+        strcpy(port, argv[1]);
+    else 
+        strcpy(port, PORT_NAME);
     printf("libmodbus modbu-rtu master demo: %s, 9600, N, 8, 1\n", port);
+
     mb = modbus_new_rtu(port, 9600, 'N', 8, 1);
+
     modbus_set_debug(mb, TRUE);
 
     if (mb == NULL)
@@ -25,6 +73,25 @@ int zd_setup(){
         printf("connect failed: %s\n", modbus_strerror(errno));
         return 0;
     }
+    while(1)
+    {
+        printf("\n\nREG_ADDRESS     : "); ret = read_value(mb, REG_ADDRESS      ); unparse_2_bytes(ret);
+        printf("\n\nREG_MICROSTEP   : "); ret = read_value(mb, REG_MICROSTEP    ); unparse_2_bytes(ret);
+        printf("\n\nREG_POS_MODE    : "); ret = read_value(mb, REG_POS_MODE     ); unparse_2_bytes(ret);
+        printf("\n\nREG_ACC_STEP_H  : "); ret = read_value(mb, REG_ACC_STEP_H   ); unparse_4_bytes(ret);
+        printf("\n\nREG_ACC_PARAM_H : "); retf = read_fvalue(mb, REG_ACC_PARAM_H); unparse_float_to_bytes(retf);       
+        printf("\n\nREG_INIT_PERIOD : "); ret = read_value(mb, REG_INIT_PERIOD  ); unparse_2_bytes(ret);
+        printf("\n\nREG_MAX_PERIOD  : "); ret = read_value(mb, REG_MAX_PERIOD   ); unparse_2_bytes(ret);
+        printf("\n\nREG_MAX_DIST_H  : "); ret = read_value(mb, REG_MAX_DIST_H   ); unparse_4_bytes(ret);
+        printf("\n\nREG_ZERO_POS_H  : "); ret = read_value(mb, REG_ZERO_POS_H   ); unparse_4_bytes(ret);
+        printf("\n\nREG_LIM_SW_OFST : "); ret = read_value(mb, REG_LIM_SW_OFST  ); unparse_2_bytes(ret);
+        sleep(1);
+    }
+
+    modbus_close(mb);
+    modbus_free(mb);
+    system("pause");
+    return 0;
 }
 
 int num_bytes(int reg_addr){
